7ca1ab4788efac1b37bd06a4aa9422fc084cb46f
GabriGat2 <gabri.naba21a@gmail.com>
2025-08-29 10:50:56 +0200

    Rinominato Form1 in FormMain

--- GAlbum/Program.cs
+++ GAlbum/Program.cs
@@ -16,7 +16,7 @@ namespace GAlbum
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
