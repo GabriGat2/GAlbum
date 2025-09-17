ea08a20bfff161088f52f632a8fa87dc17eb54f6
GabriGat2 <gabri.naba21a@gmail.com>
2025-09-17 11:28:03 +0200

    Vista directory Sorgente Destinazione

--- GAlbum/FormSelezioneFoto.Designer.cs
+++ GAlbum/FormSelezioneFoto.Designer.cs
@@ -29,24 +29,29 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.textBoxDestinazione = new System.Windows.Forms.TextBox();
-            this.butDestinazione = new System.Windows.Forms.Button();
-            this.textBoxSorgente = new System.Windows.Forms.TextBox();
-            this.butSorgente = new System.Windows.Forms.Button();
             this.groupBoxPath = new System.Windows.Forms.GroupBox();
+            this.textBoxSorgente = new System.Windows.Forms.TextBox();
+            this.textBoxDestinazione = new System.Windows.Forms.TextBox();
+            this.butSorgente = new System.Windows.Forms.Button();
+            this.butDestinazione = new System.Windows.Forms.Button();
             this.splitContainer1B2 = new System.Windows.Forms.SplitContainer();
             this.splitContainer1B2B3 = new System.Windows.Forms.SplitContainer();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.butPrecedente = new System.Windows.Forms.Button();
-            this.butSuccessiva = new System.Windows.Forms.Button();
-            this.butApri = new System.Windows.Forms.Button();
             this.textBoxPathFoto = new System.Windows.Forms.TextBox();
+            this.butPrecedente = new System.Windows.Forms.Button();
+            this.butApri = new System.Windows.Forms.Button();
+            this.butSuccessiva = new System.Windows.Forms.Button();
+            this.splitContainer1B2A3 = new System.Windows.Forms.SplitContainer();
+            this.groupBoxDestinazione = new System.Windows.Forms.GroupBox();
+            this.groupBoxSorgente = new System.Windows.Forms.GroupBox();
+            this.treeViewDestinazione = new System.Windows.Forms.TreeView();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.groupBoxPath.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1B2)).BeginInit();
+            this.splitContainer1B2.Panel1.SuspendLayout();
             this.splitContainer1B2.Panel2.SuspendLayout();
             this.splitContainer1B2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1B2B3)).BeginInit();
@@ -54,6 +59,11 @@
             this.splitContainer1B2B3.Panel2.SuspendLayout();
             this.splitContainer1B2B3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1B2A3)).BeginInit();
+            this.splitContainer1B2A3.Panel1.SuspendLayout();
+            this.splitContainer1B2A3.Panel2.SuspendLayout();
+            this.splitContainer1B2A3.SuspendLayout();
+            this.groupBoxDestinazione.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -74,41 +84,6 @@
             this.splitContainer1.SplitterDistance = 90;
             this.splitContainer1.TabIndex = 0;
             // 
-            // textBoxDestinazione
-            // 
-            this.textBoxDestinazione.Location = new System.Drawing.Point(106, 59);
-            this.textBoxDestinazione.Name = "textBoxDestinazione";
-            this.textBoxDestinazione.Size = new System.Drawing.Size(772, 20);
-            this.textBoxDestinazione.TabIndex = 12;
-            this.textBoxDestinazione.TextChanged += new System.EventHandler(this.textBoxDestinazione_TextChanged);
-            // 
-            // butDestinazione
-            // 
-            this.butDestinazione.Location = new System.Drawing.Point(10, 57);
-            this.butDestinazione.Name = "butDestinazione";
-            this.butDestinazione.Size = new System.Drawing.Size(75, 23);
-            this.butDestinazione.TabIndex = 11;
-            this.butDestinazione.Text = "Destinazione";
-            this.butDestinazione.UseVisualStyleBackColor = true;
-            this.butDestinazione.Click += new System.EventHandler(this.butDestinazione_Click);
-            // 
-            // textBoxSorgente
-            // 
-            this.textBoxSorgente.Location = new System.Drawing.Point(106, 29);
-            this.textBoxSorgente.Name = "textBoxSorgente";
-            this.textBoxSorgente.Size = new System.Drawing.Size(772, 20);
-            this.textBoxSorgente.TabIndex = 10;
-            // 
-            // butSorgente
-            // 
-            this.butSorgente.Location = new System.Drawing.Point(10, 27);
-            this.butSorgente.Name = "butSorgente";
-            this.butSorgente.Size = new System.Drawing.Size(75, 23);
-            this.butSorgente.TabIndex = 9;
-            this.butSorgente.Text = "Sorgente";
-            this.butSorgente.UseVisualStyleBackColor = true;
-            this.butSorgente.Click += new System.EventHandler(this.butSorgente_Click);
-            // 
             // groupBoxPath
             // 
             this.groupBoxPath.Controls.Add(this.textBoxSorgente);
@@ -123,11 +98,50 @@
             this.groupBoxPath.TabStop = false;
             this.groupBoxPath.Text = "Selezione di sorgente e destinazione";
             // 
+            // textBoxSorgente
+            // 
+            this.textBoxSorgente.Location = new System.Drawing.Point(106, 29);
+            this.textBoxSorgente.Name = "textBoxSorgente";
+            this.textBoxSorgente.Size = new System.Drawing.Size(772, 20);
+            this.textBoxSorgente.TabIndex = 10;
+            // 
+            // textBoxDestinazione
+            // 
+            this.textBoxDestinazione.Location = new System.Drawing.Point(106, 59);
+            this.textBoxDestinazione.Name = "textBoxDestinazione";
+            this.textBoxDestinazione.Size = new System.Drawing.Size(772, 20);
+            this.textBoxDestinazione.TabIndex = 12;
+            this.textBoxDestinazione.TextChanged += new System.EventHandler(this.textBoxDestinazione_TextChanged);
+            // 
+            // butSorgente
+            // 
+            this.butSorgente.Location = new System.Drawing.Point(10, 27);
+            this.butSorgente.Name = "butSorgente";
+            this.butSorgente.Size = new System.Drawing.Size(75, 23);
+            this.butSorgente.TabIndex = 9;
+            this.butSorgente.Text = "Sorgente";
+            this.butSorgente.UseVisualStyleBackColor = true;
+            this.butSorgente.Click += new System.EventHandler(this.butSorgente_Click);
+            // 
+            // butDestinazione
+            // 
+            this.butDestinazione.Location = new System.Drawing.Point(10, 57);
+            this.butDestinazione.Name = "butDestinazione";
+            this.butDestinazione.Size = new System.Drawing.Size(75, 23);
+            this.butDestinazione.TabIndex = 11;
+            this.butDestinazione.Text = "Destinazione";
+            this.butDestinazione.UseVisualStyleBackColor = true;
+            this.butDestinazione.Click += new System.EventHandler(this.butDestinazione_Click);
+            // 
             // splitContainer1B2
             // 
             this.splitContainer1B2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1B2.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1B2.Name = "splitContainer1B2";
+            // 
+            // splitContainer1B2.Panel1
+            // 
+            this.splitContainer1B2.Panel1.Controls.Add(this.splitContainer1B2A3);
             // 
             // splitContainer1B2.Panel2
             // 
@@ -167,6 +181,13 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // textBoxPathFoto
+            // 
+            this.textBoxPathFoto.Location = new System.Drawing.Point(12, 7);
+            this.textBoxPathFoto.Name = "textBoxPathFoto";
+            this.textBoxPathFoto.Size = new System.Drawing.Size(575, 20);
+            this.textBoxPathFoto.TabIndex = 8;
+            // 
             // butPrecedente
             // 
             this.butPrecedente.Location = new System.Drawing.Point(239, 33);
@@ -176,16 +197,6 @@
             this.butPrecedente.Text = "Precedente";
             this.butPrecedente.UseVisualStyleBackColor = true;
             this.butPrecedente.Click += new System.EventHandler(this.butPrecedente_Click);
-            // 
-            // butSuccessiva
-            // 
-            this.butSuccessiva.Location = new System.Drawing.Point(158, 33);
-            this.butSuccessiva.Name = "butSuccessiva";
-            this.butSuccessiva.Size = new System.Drawing.Size(75, 23);
-            this.butSuccessiva.TabIndex = 6;
-            this.butSuccessiva.Text = "Successiva";
-            this.butSuccessiva.UseVisualStyleBackColor = true;
-            this.butSuccessiva.Click += new System.EventHandler(this.butSuccessiva_Click);
             // 
             // butApri
             // 
@@ -197,12 +208,61 @@
             this.butApri.UseVisualStyleBackColor = true;
             this.butApri.Click += new System.EventHandler(this.butApri_Click);
             // 
-            // textBoxPathFoto
+            // butSuccessiva
             // 
-            this.textBoxPathFoto.Location = new System.Drawing.Point(12, 7);
-            this.textBoxPathFoto.Name = "textBoxPathFoto";
-            this.textBoxPathFoto.Size = new System.Drawing.Size(575, 20);
-            this.textBoxPathFoto.TabIndex = 8;
+            this.butSuccessiva.Location = new System.Drawing.Point(158, 33);
+            this.butSuccessiva.Name = "butSuccessiva";
+            this.butSuccessiva.Size = new System.Drawing.Size(75, 23);
+            this.butSuccessiva.TabIndex = 6;
+            this.butSuccessiva.Text = "Successiva";
+            this.butSuccessiva.UseVisualStyleBackColor = true;
+            this.butSuccessiva.Click += new System.EventHandler(this.butSuccessiva_Click);
+            // 
+            // splitContainer1B2A3
+            // 
+            this.splitContainer1B2A3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1B2A3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1B2A3.Name = "splitContainer1B2A3";
+            // 
+            // splitContainer1B2A3.Panel1
+            // 
+            this.splitContainer1B2A3.Panel1.Controls.Add(this.groupBoxSorgente);
+            // 
+            // splitContainer1B2A3.Panel2
+            // 
+            this.splitContainer1B2A3.Panel2.Controls.Add(this.groupBoxDestinazione);
+            this.splitContainer1B2A3.Size = new System.Drawing.Size(296, 632);
+            this.splitContainer1B2A3.SplitterDistance = 145;
+            this.splitContainer1B2A3.TabIndex = 0;
+            // 
+            // groupBoxDestinazione
+            // 
+            this.groupBoxDestinazione.Controls.Add(this.treeViewDestinazione);
+            this.groupBoxDestinazione.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxDestinazione.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxDestinazione.Name = "groupBoxDestinazione";
+            this.groupBoxDestinazione.Size = new System.Drawing.Size(147, 632);
+            this.groupBoxDestinazione.TabIndex = 0;
+            this.groupBoxDestinazione.TabStop = false;
+            this.groupBoxDestinazione.Text = "Destinazione";
+            // 
+            // groupBoxSorgente
+            // 
+            this.groupBoxSorgente.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxSorgente.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxSorgente.Name = "groupBoxSorgente";
+            this.groupBoxSorgente.Size = new System.Drawing.Size(145, 632);
+            this.groupBoxSorgente.TabIndex = 0;
+            this.groupBoxSorgente.TabStop = false;
+            this.groupBoxSorgente.Text = "Sorgente";
+            // 
+            // treeViewDestinazione
+            // 
+            this.treeViewDestinazione.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewDestinazione.Location = new System.Drawing.Point(3, 16);
+            this.treeViewDestinazione.Name = "treeViewDestinazione";
+            this.treeViewDestinazione.Size = new System.Drawing.Size(141, 613);
+            this.treeViewDestinazione.TabIndex = 0;
             // 
             // FormSelezioneFoto
             // 
@@ -218,6 +278,7 @@
             this.splitContainer1.ResumeLayout(false);
             this.groupBoxPath.ResumeLayout(false);
             this.groupBoxPath.PerformLayout();
+            this.splitContainer1B2.Panel1.ResumeLayout(false);
             this.splitContainer1B2.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1B2)).EndInit();
             this.splitContainer1B2.ResumeLayout(false);
@@ -227,6 +288,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1B2B3)).EndInit();
             this.splitContainer1B2B3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1B2A3.Panel1.ResumeLayout(false);
+            this.splitContainer1B2A3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1B2A3)).EndInit();
+            this.splitContainer1B2A3.ResumeLayout(false);
+            this.groupBoxDestinazione.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -246,5 +312,9 @@
         private System.Windows.Forms.Button butApri;
         private System.Windows.Forms.Button butSuccessiva;
         private System.Windows.Forms.TextBox textBoxPathFoto;
+        private System.Windows.Forms.SplitContainer splitContainer1B2A3;
+        private System.Windows.Forms.GroupBox groupBoxDestinazione;
+        private System.Windows.Forms.GroupBox groupBoxSorgente;
+        private System.Windows.Forms.TreeView treeViewDestinazione;
     }
 }
