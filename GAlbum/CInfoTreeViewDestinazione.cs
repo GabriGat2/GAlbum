16811bc2175ce5cdd88d4d946987b5c8ec5ee8b0
GabriGat2 <gabri.naba21a@gmail.com>
2025-09-29 14:55:11 +0200

    Creata la classe CInfoTreeViewDestinazione

---
+++ GAlbum/CInfoTreeViewDestinazione.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAlbum
+{
+    public class CInfoTreeViewDestinazione : CInfoTreeView
+    { // ==================================================================================================================
+        // Proprietà
+        // ==================================================================================================================
+
+
+
+        // ==================================================================================================================
+        // Metodi
+        // ==================================================================================================================
+        /// <summary>
+        /// costruttore
+        /// </summary>
+        public CInfoTreeViewDestinazione()
+        {
+
+        }
+    }// fine class CInfoTreeViewDestinazione
+}// fine namespace GAlbum
+
