990046552737e3f33fa1c4a4e4bb2f41c11f878b
GabriGat2 <gabri.naba21a@gmail.com>
2025-11-12 11:25:33 +0100

    creato form seleziona per data 2

--- GAlbum/FormSelezionaPerData.Designer.cs
+++ GAlbum/FormSelezionaPerData.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormSelezionaPerData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormSelezionaPerData";
+            this.Name = "FormSelezionaPerData";
+            this.Text = "Form Seleziona Per Data";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
