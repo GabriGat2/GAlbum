636af4d3515896fe362b1bf6d9f7bd33203550c1
GabriGat2 <gabri.naba21a@gmail.com>
2025-12-03 11:35:58 +0100

    Compressione nome con filtro accenti

--- GAlbum/CNomeLibro.cs
+++ GAlbum/CNomeLibro.cs
@@ -17,7 +17,8 @@ namespace GAlbum
         /// <summary>
         /// Nome esplicito autore 1
         /// </summary>
-        public string Autore1 { get => autore1; set =>  SetAutore1 (value); }
+        //public string Autore1 { get => autore1; set =>  SetAutore1 (value); }
+        public string Autore1 { get => autore1; set => autore1 = SetNome(value, ref Autore1Cmp); }
         private string autore1;
         /// <summary>
         ///  nome compresso autore 1 
@@ -33,7 +34,19 @@ namespace GAlbum
         private string nomeFileLibro;
 
 
-
+        // ------------------------------------------------------------------------------------------------------------------
+        // Caratteri accentati 
+        private const string Separatori = " '-";
+        private const string accenti_A = "á Á à À â Â å Å ã Ã ä Ä æ Æ";
+        private const string accenti_B = "ß";
+        private const string accenti_C = "ç Ç";
+        private const string accenti_E = "é É è È ê Ê ë Ë";
+        private const string accenti_I = "í Í ì Ì î Î ï Ï";
+        private const string accenti_N = "ñ Ñ";
+        private const string accenti_O = "ó Ó ò Ò ô Ô ø Ø õ Õ ö Ö";
+        private const string accenti_U = "ú Ú ù Ù û Û ü Ü";
+        private const string accenti_Y = "ÿ";
+        
 
         // ==================================================================================================================
         /// <summary>
@@ -85,6 +98,29 @@ namespace GAlbum
             nomeFileLibro = "==>" + Autore1Cmp + "<==";
         }
         /// <summary>
+        /// Assegna il nome e il nome compresso
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <param name="nomeCMP"></param>
+        /// <returns></returns>
+        private string SetNome(string nome, ref string nomeCmp)
+        {
+            // Comprime nome
+            nomeCmp = ComprimiNome(nome);
+
+            // Compone nome file libro 
+            ComponeNomeFileLibro();
+
+            return nome;
+        }
+        /// <summary>
+        /// Compone nome file libro 
+        /// </summary>
+        private void ComponeNomeFileLibro()
+        {
+            nomeFileLibro = "==>" + Autore1Cmp + "<==";
+        }
+        /// <summary>
         /// comprime il nome 
         /// </summary>
         /// <param name="nome"></param>
@@ -104,19 +140,113 @@ namespace GAlbum
             // Analiza i caratteri di nome 2  
             foreach (char c in nome2) 
             {
-                // analizza gli spazi
-                if (c == ' ')
+                    // analizza i separatori
+                //if (c == ' ')
+                if (Separatori.IndexOf(c) >= 0)
                 {
-                    // se arriva qui é uno spazio 
+                    // se arriva qui é un separatore 
                     maiuscolo = true;
                 }
-                else
+                    // analizza i numeri
+                else if ((c >= '0') && (c <= '9'))
                 {
-                    // se arriva qui non é uno spazio
+                    nomeCmp += c;
+                    maiuscolo = false;
+                }
+                    // analizza le lettere
+                else if ((c >= 'a') && (c <= 'z'))
+                {
                     if (maiuscolo)
                         nomeCmp += c.ToString().ToUpper();
                     else
                         nomeCmp += c;
+                    maiuscolo = false;
+                }
+                    // Analizza accento sulla A
+                else if (accenti_A.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'A';
+                    else
+                        nomeCmp += 'a';
+                    maiuscolo = false;
+                }
+                // Analizza accento sulla B
+                else if (accenti_B.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'B';
+                    else
+                        nomeCmp += 'b';
+                    maiuscolo = false;
+                }
+                // Analizza accento sulla C
+                else if (accenti_C.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'C';
+                    else
+                        nomeCmp += 'c';
+                    maiuscolo = false;
+                }
+                // Analizza accento sulla E
+                else if (accenti_E.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'E';
+                    else
+                        nomeCmp += 'e';
+                    maiuscolo = false;
+                }
+                // Analizza accento sulla I
+                else if (accenti_I.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'I';
+                    else
+                        nomeCmp += 'i';
+                    maiuscolo = false;
+                }
+                // Analizza accento sulla N
+                else if (accenti_N.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'N';
+                    else
+                        nomeCmp += 'n';
+                    maiuscolo = false;
+                }
+                // Analizza accento sulla O
+                else if (accenti_O.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'O';
+                    else
+                        nomeCmp += 'o';
+                    maiuscolo = false;
+                }
+                // Analizza accento sulla O
+                else if (accenti_U.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'U';
+                    else
+                        nomeCmp += 'u';
+                    maiuscolo = false;
+                }
+                // Analizza accento sulla Y
+                else if (accenti_Y.IndexOf(c) >= 0)
+                {
+                    if (maiuscolo)
+                        nomeCmp += 'Y';
+                    else
+                        nomeCmp += 'y';
+                    maiuscolo = false;
+                }
+                // carattere sconosciuto 
+                else
+                {
+                    nomeCmp += '°';
                     maiuscolo = false;
                 }
 
